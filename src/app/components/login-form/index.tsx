57866f18d41389cdc9a26d07627abf4d59ee6627
siwulus <siwulus@gmail.com>
2025-06-30 09:35:37 +0200

    Enhance LoginForm component by updating label class for improved styling and accessibility

--- src/app/components/login-form/index.tsx
+++ src/app/components/login-form/index.tsx
@@ -35,29 +35,29 @@ export const LoginForm: FC = () => {
           <fieldset className='fieldset bg-base-200 border-base-300 rounded-box border p-4'>
             <legend className='fieldset-legend'>Logowanie</legend>
 
-            <label className='label' htmlFor={`${id}-password`}>
+            <label className='floating-label' htmlFor={`${id}-password`}>
               <span>
                 Hasło dostępowe
                 <span className='text-error ml-1' aria-hidden='true'>
                   *
                 </span>
               </span>
-            </label>
 
-            <input
-              type='password'
-              name='password'
-              placeholder='Wprowadź hasło dostępowe'
-              id={`${id}-password`}
-              disabled={pending}
-              required
-              aria-required='true'
-              aria-invalid={!!state?.errors}
-              aria-describedby={
-                !!state?.errors ? `${id}-password-error` : undefined
-              }
-              className={`input w-full ${!!state?.errors ? 'input-error' : ''}`}
-            />
+              <input
+                type='password'
+                name='password'
+                placeholder='Wprowadź hasło dostępowe'
+                id={`${id}-password`}
+                disabled={pending}
+                required
+                aria-required='true'
+                aria-invalid={!!state?.errors}
+                aria-describedby={
+                  !!state?.errors ? `${id}-password-error` : undefined
+                }
+                className={`input w-full ${!!state?.errors ? 'input-error' : ''}`}
+              />
+            </label>
 
             {!!state?.errors?.password && (
               <label className='label'>
